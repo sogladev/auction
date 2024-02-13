10d4bec8aa51c99dc3832a6f2586b697e197f78f
Ella <ella36anon@gmail.com>
2024-02-13 08:58:17 +0100

    Can route to /#/room

---
+++ frontend/auction/src/pages/RoomPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <example-component
+    title="Example component"
+    active
+    :todos="todos"
+    :meta="meta"
+  ></example-component>
+  <h1>Welcome to a Room</h1>
+  <p>This is the main room page where auctions happen</p>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { ExampleComponent },
+  setup() {
+    const todos = ref<Todo[]>([
+      {
+        id: 1,
+        content: 'ct1',
+      },
+    ]);
+    const meta = ref<Meta>({
+      totalCount: 1200,
+    });
+    return { todos, meta };
+  },
+});
+</script>
--- frontend/auction/src/router/routes.ts
+++ frontend/auction/src/router/routes.ts
@@ -3,10 +3,21 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
 
+    component: () => import('layouts/MainLayout.vue'),
+
+    children: [
+      {
+        path: 'room', // here it is, route /user/posts
+        component: import('pages/RoomPage.vue'), // we reference /src/pages/Posts.vue imported above
+      },
+      {
+        path: '', // here it is, route /user/profile
+        component: import('pages/IndexPage.vue'), // we reference /src/pages/Profile.vue imported above
+      },
+
+    ],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
