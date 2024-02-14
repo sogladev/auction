e39cee11a626c7c18a7f8c1eb9045f0e2abaae76
Ella <ella36anon@gmail.com>
2024-02-14 04:34:17 +0100

    use q-cards for form

--- frontend/auction/src/components/IndexForm.vue
+++ frontend/auction/src/components/IndexForm.vue
@@ -1,121 +1,139 @@
 <template>
-  <q-form ref="for">
-    <q-input
-      v-model="lootmaster"
-      label="Lootmaster"
-      :rules="[
-        (val) => typeof val == 'string' || 'Name must be a string',
-        (val) =>
-          /^[a-zA-Z0-9]{0,12}$/.test(val) ||
-          'Name can only contain alphanumeric characters and be max 12 chars',
-      ]"
-    />
-    <q-input
-      v-model.number="organiserFee"
-      type="number"
-      label="Organiser fee (%)"
-      prefix="%"
-      min="0"
-      max="100"
-      :rules="[
-        (val) =>
-          (!isNaN(val) && val <= 100 && val >= 0) ||
-          'Fee must be a number between 0 and 100!',
-      ]"
-    />
-    <q-toggle
-      v-model="enableDiscordProtection"
-      checked-icon="check"
-      color="green"
-      label="Enable Discord verification"
-      unchecked-icon="clear"
-      size="lg"
-    />
-    <div class="q-pa-md">
-      <q-icon name="timer" />
-      <q-badge color="primary">
-        Bid Duration {{ bidDurationInSeconds }}s
-        {{ formatTime(bidDurationInSeconds) }}(MM:SS)
-      </q-badge>
+  <q-page class="fit row wrap content-start items-center justify-center">
+    <q-card class="create-room-card">
+      <q-form ref="for">
+        <q-card-section>
+          <div class="text-h6">Create New Room</div>
+          <q-input
+            v-model="lootmaster"
+            label="Lootmaster"
+            :rules="[
+              (val) => typeof val == 'string' || 'Name must be a string',
+              (val) =>
+                /^[a-zA-Z0-9]{0,12}$/.test(val) ||
+                'Name can only contain alphanumeric characters and be max 12 chars',
+            ]"
+          />
+          <q-input
+            v-model.number="organiserFee"
+            type="number"
+            label="Organiser fee (%)"
+            prefix="%"
+            min="0"
+            max="100"
+            :rules="[
+              (val) =>
+                (!isNaN(val) && val <= 100 && val >= 0) ||
+                'Fee must be a number between 0 and 100!',
+            ]"
+          />
+        </q-card-section>
 
-      <q-slider
-        v-model="bidDurationInSeconds"
-        :min="0"
-        :max="720"
-        :step="5"
-        label
-        :label-value="formatTime(bidDurationInSeconds)"
-        color="primary"
-      />
-    </div>
+        <q-card-section>
+          <div class="text-h6">Security</div>
+          <q-toggle
+            v-model="enableDiscordProtection"
+            checked-icon="check"
+            color="green"
+            label="Enable Discord verification"
+            unchecked-icon="clear"
+          />
+        </q-card-section>
 
-    <div class="q-pa-md">
-      <q-icon name="timer" />
-      <q-badge color="primary">
-        Countdown Duration {{ countDownTimeInSeconds }}s
-        {{ formatTime(countDownTimeInSeconds) }}(MM:SS)
-      </q-badge>
+        <q-card-section>
+          <div class="text-h6">Session Settings</div>
+          <div class="q-pa-md">
+            <q-icon name="timer" />
+            <q-badge color="primary">
+              Bid Duration {{ bidDurationInSeconds }}s
+              {{ formatTime(bidDurationInSeconds) }}(MM:SS)
+            </q-badge>
 
-      <q-slider
-        v-model="countDownTimeInSeconds"
-        :min="20"
-        :max="120"
-        :step="5"
-        label
-        :label-value="formatTime(countDownTimeInSeconds)"
-        color="primary"
-      />
-    </div>
+            <q-slider
+              v-model="bidDurationInSeconds"
+              :min="0"
+              :max="720"
+              :step="5"
+              label
+              :label-value="formatTime(bidDurationInSeconds)"
+              color="primary"
+            />
+          </div>
 
-    <q-input
-      v-model.number="minimumBid"
-      type="number"
-      label="Minimum bid"
-      min="0"
-      :rules="[
-        (val) =>
-          (!isNaN(val) && val >= 0) || 'Min bid must be a positive numberl!',
-      ]"
-    />
+          <div class="q-pa-md">
+            <q-icon name="timer" />
+            <q-badge color="primary">
+              Countdown Duration {{ countDownTimeInSeconds }}s
+              {{ formatTime(countDownTimeInSeconds) }}(MM:SS)
+            </q-badge>
 
-    <q-input
-      v-model.number="minimumBidIncrement"
-      type="number"
-      label="Minimum increment"
-      min="1"
-      :rules="[
-        (val) =>
-          (!isNaN(val) && val >= 0) || 'Field must be a number greater than 0!',
-      ]"
-    />
+            <q-slider
+              v-model="countDownTimeInSeconds"
+              :min="20"
+              :max="120"
+              :step="5"
+              label
+              :label-value="formatTime(countDownTimeInSeconds)"
+              color="primary"
+            />
+          </div>
 
-    <q-toggle
-      v-model="restrictBidsToEquipable"
-      color="primary"
-      label="Restrict bids to equipable items"
-    />
-    <q-toggle
-      v-model="hideNameOfHighestBidder"
-      color="primary"
-      label="Hide name of highest bidder"
-    />
-    <q-toggle
-      v-model="hidePayoutDetails"
-      color="primary"
-      label="Hide payout details"
-    />
+          <q-input
+            v-model.number="minimumBid"
+            type="number"
+            label="Minimum bid"
+            min="0"
+            :rules="[
+              (val) =>
+                (!isNaN(val) && val >= 0) ||
+                'Min bid must be a positive numberl!',
+            ]"
+          />
 
-    <div>
-      <q-btn
-        unelevated
-        @click="onSubmit"
-        type="submit"
-        color="primary"
-        label="Create room"
-      />
-      <q-btn unelevated type="reset" color="primary" label="Reset" />
-    </div>
-  </q-form>
+          <q-input
+            v-model.number="minimumBidIncrement"
+            type="number"
+            label="Minimum increment"
+            min="1"
+            :rules="[
+              (val) =>
+                (!isNaN(val) && val >= 0) ||
+                'Field must be a number greater than 0!',
+            ]"
+          />
+        </q-card-section>
+
+        <q-card-section>
+          <div class="text-h6">Advanced Settings</div>
+          <q-toggle
+            v-model="restrictBidsToEquipable"
+            color="primary"
+            label="Restrict bids to equipable items"
+          />
+          <q-toggle
+            v-model="hideNameOfHighestBidder"
+            color="primary"
+            label="Hide name of highest bidder"
+          />
+          <q-toggle
+            v-model="hidePayoutDetails"
+            color="primary"
+            label="Hide payout details"
+          />
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn
+            unelevated
+            @click="onSubmit"
+            type="submit"
+            color="primary"
+            label="Create room"
+          />
+          <q-btn unelevated type="reset" color="primary" label="Reset" />
+        </q-card-actions>
+      </q-form> </q-card
+  ></q-page>
 </template>
 
 <script lang="ts">
