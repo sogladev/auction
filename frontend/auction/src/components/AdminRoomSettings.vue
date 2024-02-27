e12999471c546e1724ddab5410fb46bac7df9528
Jelle Meeus <jellemeeus@hotmail.com>
2024-02-27 20:58:23 +0100

    Split admin settings in its own component

--- frontend/auction/src/components/AdminRoomSettings.vue
+++ frontend/auction/src/components/AdminRoomSettings.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- justify-around or justiy-between -->
-  <q-card class="create-room-card" style="min-width: 25vw">
-    <q-card-section class="justify-left" style="width: 50%">
+  <q-card class="admin-room-card">
+    <q-card-section class="justify-left">
       <q-field bg-color="primary" filled label="Admin secret key (click to copy)" stack-label>
         <template v-slot:prepend>
           <q-icon name="key" />
@@ -14,7 +14,6 @@
         </template>
       </q-field>
     </q-card-section>
-
 
     <q-form ref="for" @submit.prevent="onSubmit">
       <q-card-section>
@@ -113,8 +112,27 @@
         <q-btn unelevated type="submit" color="primary" label="Update settings" />
       </q-card-actions>
     </q-form>
-  </q-card>
 
+    <q-form ref="for" @submit.prevent="onSubmitUpdateItems">
+      <q-card-section>
+        <div class="text-h6">Items</div>
+        <q-card-section class="justify-around">
+          <div class="text-h7">
+            Import items by pasting your import string
+          </div>
+          <q-input max debounce="500" label="Paste your string here" v-model="csvString" filled type="textarea" :rules="[
+            (val) =>
+              (typeof val == 'string' &&
+                val.startsWith(validationHeader)) ||
+              'Invalid import string. Copy all output from /hlm e!',
+          ]" />
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn unelevated type="submit" color="primary" label="Update items" />
+        </q-card-actions>
+      </q-card-section>
+    </q-form>
+  </q-card>
   <q-ajax-bar ref="bar" position="bottom" color="accent" size="10px" skip-hijack />
 </template>
 
@@ -124,12 +142,34 @@ import { reactive, ref } from 'vue';
 import { useQuasar } from 'quasar';
 import { api } from 'boot/axios';
 import { copyToClipboard } from 'quasar'
+import Papa from 'papaparse';
 
 const bar = ref(null); // ajax bar
 const adminKey = ref('this is some key'); // ajax bar
 const $q = useQuasar();
 const route = useRoute();
 const roomId = route.params.id;
+
+const validationHeader =
+  'rowId,id,name,quality,ilvl,minLevel,itemType,itemSubType,infoStatus,infoMinPrice,guid';
+
+const debugImportString = `rowId,id,name,quality,ilvl,minLevel,itemType,itemSubType,infoStatus,infoMinPrice,guid
+1,19137,Onslaught Girdle,4,78,60,Armor,Plate,1,3000,noguid
+2,18814,Choker of the Fire Lord,4,78,60,Armor,Miscellaneous,1,3000,noguid
+3,17076,Bonereaver's Edge,4,77,60,Weapon,Two-Handed Swords,1,3000,noguid
+4,12282,Worn Battleaxe,1,2,1,Weapon,Two-Handed Axes,1,3000,Item-5827-0-40000000C90648E0
+`;
+
+const csvString = ref(debugImportString);
+
+async function onSubmitUpdateItems() {
+  console.log('@submet.prevent on submit roomsring');
+  const output = Papa.parse(csvString.value);
+  console.log(output);
+  console.log('TODO: Create session from import string');
+  // TODO: Create session from import string
+  // Populate "Auctions" data and start session
+}
 
 // TODO: Use Model type instead
 type RoomSettingsFormState = {
@@ -166,7 +206,7 @@ const formState = reactive<RoomSettingsFormState>({
 
 async function onSubmit() {
   console.log('@submet.prevent');
-  console.log('Post form and create room with given settings');
+  console.log('Put form and create room with given settings');
   console.log(formState);
   api
     .put(`/api/rooms/${roomId}`, formState)
--- frontend/auction/src/pages/RoomPage.vue
+++ frontend/auction/src/pages/RoomPage.vue
@@ -1,72 +1,25 @@
 <template>
   <div class="flex flex-center column">
     <div class="text-h6" style="text-align:center">You are in Room {{ roomId }}</div>
-    <q-card class="create-session-card">
-
-      <div class="text-h6">Admin Settings</div>
-      <RoomSettingsForm />
-      <q-form ref="for" @submit.prevent="onSubmitUpdateItems">
-        <q-card-section>
-          <div class="text-h6">Items</div>
-          <q-card-section class="justify-around">
-            <div class="text-h7">
-              Import items by pasting your import string
-            </div>
-            <q-input max debounce="500" label="Paste your string here" v-model="csvString" filled type="textarea" :rules="[
-              (val) =>
-                (typeof val == 'string' &&
-                  val.startsWith(validationHeader)) ||
-                'Invalid import string. Copy all output from /hlm e!',
-            ]" />
-          </q-card-section>
-          <q-card-actions align="right">
-            <q-btn unelevated type="submit" color="primary" label="Update items" />
-          </q-card-actions>
-        </q-card-section>
-      </q-form>
-
-      <div class="text-h6">Session</div>
-      <SessionApp />
+    <q-card class="room-card">
+      <div class="admin-room-settings">
+        <div class="text-h6">Admin Settings</div>
+        <AdminRoomSettings />
+      </div>
+      <div class="session-app">
+        <div class="text-h6">Session</div>
+        <SessionApp />
+      </div>
     </q-card>
-
-    <q-ajax-bar ref="bar" position="bottom" color="accent" size="10px" skip-hijack />
   </div>
 </template>
 
 <script lang="ts" setup>
-import RoomSettingsForm from 'components/RoomSettingsForm.vue';
-import Papa from 'papaparse';
-
-import { ref } from 'vue';
-
-import { useRoute } from 'vue-router';
-const route = useRoute();
-const roomId = route.params.id;
-
+import AdminRoomSettings from 'components/AdminRoomSettings.vue';
 import SessionApp from 'components/SessionApp.vue';
 
-//const router = useRouter();
-const bar = ref(null); // ajax bar
+import { useRoute } from 'vue-router';
 
-const validationHeader =
-  'rowId,id,name,quality,ilvl,minLevel,itemType,itemSubType,infoStatus,infoMinPrice,guid';
-
-const debugImportString = `rowId,id,name,quality,ilvl,minLevel,itemType,itemSubType,infoStatus,infoMinPrice,guid
-1,19137,Onslaught Girdle,4,78,60,Armor,Plate,1,3000,noguid
-2,18814,Choker of the Fire Lord,4,78,60,Armor,Miscellaneous,1,3000,noguid
-3,17076,Bonereaver's Edge,4,77,60,Weapon,Two-Handed Swords,1,3000,noguid
-4,12282,Worn Battleaxe,1,2,1,Weapon,Two-Handed Axes,1,3000,Item-5827-0-40000000C90648E0
-`;
-
-const csvString = ref(debugImportString);
-
-async function onSubmitUpdateItems() {
-  console.log('@submet.prevent on submit roomsring');
-  const output = Papa.parse(csvString.value);
-  console.log(output);
-  console.log('TODO: Create session from import string');
-  // TODO: Create session from import string
-  // Populate "Auctions" data and start session
-}
-
+const route = useRoute();
+const roomId = route.params.id;
 </script>
