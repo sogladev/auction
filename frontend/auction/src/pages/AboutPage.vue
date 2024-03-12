2eb87f93a4100c54326f31452565431ae7311505
Jelle Meeus <jellemeeus@hotmail.com>
2024-03-12 17:20:34 +0100

    refactor: about page

--- frontend/auction/src/pages/AboutPage.vue
+++ frontend/auction/src/pages/AboutPage.vue
@@ -4,23 +4,12 @@
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
   components: { },
   setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1',
-      },
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200,
-    });
-    return { todos, meta };
   },
 });
 </script>
