faa9c733dd4186c53f91da3a0b70d43dc3326c86
Jelle Meeus <jellemeeus@hotmail.com>
2024-03-12 20:04:20 +0100

    feat: Show namespace image on room and fix column spacing

---
+++ frontend/auction/src/components/NamespaceImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-img ratio="1" fit="contain" :src="getSrc(namespace)" spinner-color="white">
+  </q-img>
+</template>
+
+<script setup lang="ts">
+import { Namespace } from 'src/components/models';
+import { ref } from 'vue';
+
+const namespace = ref(Namespace.Era);
+
+function getSrc(namespace: Namespace) {
+  if (namespace == Namespace.Era) {
+    return '/src/assets/wow-logo/wow-classic_1_1_240.png'
+  }
+  else if (namespace == Namespace.Retail) {
+    return '/src/assets/wow-logo/wow-dragonflight_1_1_240.png'
+
+  }
+  return '/src/assets/wow-logo/wow-wrath_1_1_240.png'
+}
+</script>
+
+<style>
+</style>
--- frontend/auction/src/pages/RoomPage.vue
+++ frontend/auction/src/pages/RoomPage.vue
@@ -1,71 +1,84 @@
 <template>
-  <q-page padding>
-    <div v-if="isValidRoom == null">
-      <q-spinner color="primary" size="10em" />
-    </div>
-
-    <div v-if="isValidRoom === false">
-      <q-field bg-color="red" filled>
-        <template v-slot:prepend>
-          <q-icon name="error" />
-        </template>
-        <template v-slot:control>
-          <div class="self-center full-width no-outline text-h6" tabindex="0">Invalid Room!</div>
-        </template>
-      </q-field>
-    </div>
-
-    <div v-if="isValidRoom" class="room">
-      <div class="text-h4" style="text-align:center">You are in Room</div>
-      <div class="text-h5" style="text-align:center">{{ roomId }}</div>
-      <div>
-        <div>
-          <q-field bg-color="green" filled>
-            <template v-slot:prepend>
-              <q-icon name="check" />
-            </template>
-            <template v-slot:control>
-              <div class="self-center full-width no-outline text-h6" tabindex="0"> Valid Room </div>
-            </template>
-          </q-field>
-        </div>
+  <q-page class="row">
+    <div class="col-2 px-0"></div>
+    <div class="col-8 q-mt-md">
+      <div v-if="isValidRoom == null">
+        <q-spinner color="primary" size="10em" />
       </div>
-
-      <div class="justify-left">
-        <q-toggle v-model="isShowAdminControls" color="warning" label="Show admin controls" size="lg" />
-        <q-field bg-color="warning" color="warning" filled label="Admin secret key" stack-label>
+      <div v-if="isValidRoom === false">
+        <q-field bg-color="red" filled>
           <template v-slot:prepend>
-            <q-icon name="key" />
+            <q-icon name="error" />
           </template>
-
-          <template v-slot:append>
-            <q-btn icon="content_copy" @click="copyToClipboard(adminKey)" />
-          </template>
-
           <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0">{{ adminKey }}</div>
+            <div class="self-center full-width no-outline text-h6" tabindex="0">Invalid Room!</div>
           </template>
         </q-field>
-
-        <q-toggle v-model="isAdmin" color="warning" label="Test admin" size="lg" />
       </div>
 
-      <div>
-        <div>
-          <div v-if="isAdmin && isShowAdminControls">
-            <div class="text-h6">Admin Settings</div>
-            <div class="text-h7">configure settings, click save to update to db</div>
-            <AdminRoomSettings />
-            <q-separator spaced inset />
+      <div v-if="isValidRoom" class="room">
+        <div class="row">
+          <div class="col-8">
+            <div class="text-h4" style="text-align:left">You are in Room</div>
+            <div class="text-h5" style="text-align:left">{{ roomId }}</div>
           </div>
+          <div class="col-4">
+            <NamespaceImage style="max-width: 9em;" :namespace="Namespace.Era" />
+          </div>
+        </div>
+
+
+        <div>
           <div>
-            <div class="text-h6">Session</div>
-            <div class="text-h7">Shows settings, button to synchronize with database, auction displays auctions</div>
-            <SessionApp />
+            <q-field bg-color="green" filled>
+              <template v-slot:prepend>
+                <q-icon name="check" />
+              </template>
+              <template v-slot:control>
+                <div class="self-center full-width no-outline text-h6" tabindex="0"> Valid Room </div>
+              </template>
+            </q-field>
+          </div>
+        </div>
+
+        <div class="justify-left">
+          <q-toggle v-model="isShowAdminControls" color="warning" label="Show admin controls" size="lg" />
+          <q-field bg-color="warning" color="warning" filled label="Admin secret key" stack-label>
+            <template v-slot:prepend>
+              <q-icon name="key" />
+            </template>
+
+            <template v-slot:append>
+              <q-btn icon="content_copy" @click="copyToClipboard(adminKey)" />
+            </template>
+
+            <template v-slot:control>
+              <div class="self-center full-width no-outline" tabindex="0">{{ adminKey }}</div>
+            </template>
+          </q-field>
+
+          <q-toggle v-model="isAdmin" color="warning" label="Test admin" size="lg" />
+        </div>
+
+        <div>
+          <div>
+            <div v-if="isAdmin && isShowAdminControls">
+              <div class="text-h6">Admin Settings</div>
+              <div class="text-h7">configure settings, click save to update to db</div>
+              <AdminRoomSettings />
+              <q-separator spaced inset />
+            </div>
+            <div>
+              <div class="text-h6">Session</div>
+              <div class="text-h7">Shows settings, button to synchronize with database, auction displays auctions</div>
+              <SessionApp />
+            </div>
           </div>
         </div>
       </div>
+
     </div>
+    <div class="col-2 px-0"></div>
   </q-page>
 </template>
 
@@ -79,6 +92,9 @@ import { useRoomStore } from 'src/stores/RoomStore';
 
 import { useRoute } from 'vue-router';
 import { storeToRefs } from 'pinia';
+
+import NamespaceImage from 'src/components/NamespaceImage.vue'
+import { Namespace } from 'src/components/models';
 
 
 const route = useRoute();
@@ -101,6 +117,7 @@ else {
 
 <style lang="scss">
 @import '/src/css/app.scss';
+
 .room {
   margin: 0 auto;
   padding: 20px 10px;
